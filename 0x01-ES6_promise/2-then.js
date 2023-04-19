e8c1af1d1aaa17d49a22ce0f842c37640391f62f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-19 22:46:04 +0100

    Fix 2 for task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const myPromise = promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => (new Error))
+    .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
   // Return Promise
   return myPromise;
