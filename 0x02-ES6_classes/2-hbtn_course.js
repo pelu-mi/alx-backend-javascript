dd2adea1b4be37060270f0b613d6505a649c4e39
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-20 16:42:25 +0100

    Fix 1 for task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -19,4 +19,40 @@ export default class HolbertonCourse {
       throw new TypeError('Students must be an array of strings');
     }
   }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  // Getter and Setter for length
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  // Getter and Setter for students
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
 }
