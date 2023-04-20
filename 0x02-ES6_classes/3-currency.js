f17526e5ae43fe47326516faca720df82f5e5aa9
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-20 16:55:36 +0100

    Added file for task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currencu {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  // displayFullCurrency - return attributes in string format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
