e3ea42d4c6aecbba57e052eb3bb22a65b8acf8b5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-20 02:15:26 +0100

    Fix 1 for task 10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,27 +1,14 @@
 /* eslint-disable prefer-destructuring */
 import { uploadPhoto, createUser } from './utils';
 
-export default function asyncUploadUser() {
-  /*
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const body = values[0].body;
-      const first = values[1].firstName;
-      const last = values[1].lastName;
-      console.log(`${body} ${first} ${last}`);
-    })
-    .catch(() => { console.log('Signup system offline'); });
-  */
-  const result = {
-    photo: null,
-    user: null,
-  };
-
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      result.photo = values[0];
-      result.user = values[1];
-      return result;
-    })
-    .catch(() => result);
+export default async function asyncUploadUser() {
+  let result = {};
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
+  } catch (error) {
+    result = { photo: null, user: null };
+  }
+  return result;
 }
