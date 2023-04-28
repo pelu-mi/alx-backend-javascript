8cbd7b7fa8adaa69c7808245dec030adfed99e08
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-28 21:17:18 +0100

    Added file for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-param-reassign */
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const uint8view = new Uint8Array(buffer);
   try {
+    const buffer = new ArrayBuffer(length);
+    const uint8view = new Uint8Array(buffer);
     uint8view[position] = value;
   } catch (err) {
     throw new Error('Position outside range');
