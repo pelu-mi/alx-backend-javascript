2ff8c869ff966ecbc1df03242b033b5b29893d8c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-28 21:19:38 +0100

    Fix 2 for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
     const uint8view = new Uint8Array(buffer);
     uint8view[position] = value;
+    return new DataView(buffer);
   } catch (err) {
     throw new Error('Position outside range');
   }
-  return new DataView(buffer);
 }
