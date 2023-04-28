2a05c10e4e0433b2683fce19440e3af5c5edda29
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-28 21:15:03 +0100

    ESlint fix for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
   const uint8view = new Uint8Array(buffer);
   try {
     uint8view[position] = value;
-  } catch(err) {
+  } catch (err) {
     throw new Error('Position outside range');
   }
   return new DataView(buffer);
