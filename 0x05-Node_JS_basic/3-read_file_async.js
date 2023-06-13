dd927a87ae4f2fc8de1a81372144a566ec6bcbfa
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-13 23:08:37 +0100

    Fix 1 for task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,34 +2,38 @@
 const fs = require('fs');
 
 const countStudents = (path) => new Promise((resolve, reject) => {
+  if (!path) { reject(new Error('Cannot load the database')); }
   fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) { throw new Error('Cannot load the database'); }
-    if (!(data)) { return; }
-    const dataArray = data.trim().split('\n').slice(1);
-    console.log(`Number of students: ${dataArray.length}`);
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      const logs = [];
+      const studentsArray = data.trim().split('\n').slice(1);
+      const studentNumber = `Number of students: ${studentsArray.length}`;
 
-    // Find all the different fields in the database and count them
-    const fields = {};
-    for (const record of dataArray) {
-      const field = record.split(',')[3];
-      if (!fields[field]) {
-        fields[field] = 0;
-      }
-      fields[field] += 1;
-    }
+      logs.push(studentNumber);
+      console.log(studentNumber);
 
-    // Print out all the first names per field using the format
-    for (const [field, fVal] of Object.entries(fields)) {
-      process.stdout.write(`Number of students in ${field}: ${fVal}. List: `);
-      let flag = 0;
-      for (const record of dataArray) {
-        if (record.split(',')[3] === field) {
-          if (flag === 1) { process.stdout.write(', '); }
-          process.stdout.write(`${record.split(',')[0]}`);
-          flag = 1;
-        }
-      }
-      process.stdout.write('\n');
+      const students = studentsArray.map((student) => {
+        const fields = student.replace('\r', '').split(',');
+        return fields;
+      });
+
+      const categories = [...new Set(students.map((student) => student[student.length - 1]))];
+
+      categories.forEach((category) => {
+        const filteredStudents = students
+          .filter((student) => student[student.length - 1] === category)
+          .map((student) => student[0]);
+
+        const categoryInfo = `Number of students in ${category
+        }: ${filteredStudents.length}. List: ${filteredStudents.join(', ')}`;
+
+        logs.push(categoryInfo);
+        console.log(categoryInfo);
+
+        resolve(logs);
+      });
     }
   });
 });
