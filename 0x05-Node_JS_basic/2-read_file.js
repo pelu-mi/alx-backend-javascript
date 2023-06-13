10af919f580563126384bfc7984323d572aee604
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-13 15:30:17 +0100

    Eslint fix for task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,18 @@
-module.exports = function countStudents(path) {
-  const fs = require('fs');
+/* eslint-disable no-const-assign */
+const fs = require('fs');
 
-  fs.readFile(path, 'utf-8', (err, data) => {
+module.exports = function countStudents(path) {
+  fs.readFileSync(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
-    const data_array = data.split("\n").slice(1, 11);
-    console.log(`Number of students: ${data_array.length}`);
+    const dataArray = data.split('\n').slice(1, 11);
+    console.log(`Number of students: ${dataArray.length}`);
 
     // Find all the different fields in the database and count them
-    let fields = {};
-    for (let record of data_array) {
-      let field = record.split(',')[3];
+    const fields = {};
+    for (const record of dataArray) {
+      const field = record.split(',')[3];
       if (!fields[field]) {
         fields[field] = 0;
       }
@@ -21,13 +22,13 @@ module.exports = function countStudents(path) {
     // Print out all the first names per field using the format
     for (const [field, fVal] of Object.entries(fields)) {
       process.stdout.write(`Number of students in ${field}: ${fVal}. List: `);
-      let flag = 0;
-      for (let record of data_array) {
-        if (record.split(',')[3] == field){
-          if (flag == 1) { process.stdout.write(', ') }
-	  process.stdout.write(`${record.split(',')[0]}`);
+      const flag = 0;
+      for (const record of dataArray) {
+        if (record.split(',')[3] === field) {
+          if (flag === 1) { process.stdout.write(', '); }
+          process.stdout.write(`${record.split(',')[0]}`);
           flag = 1;
-	}
+        }
       }
       process.stdout.write('\n');
     }
