e62b180f2f02dce16def98e9419b4bf9dc7c1661
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-13 23:21:25 +0100

    Added file for task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,8 +3,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-// Add code from task 3 here
+// Import code from task 3 here
+const countStudents = require('./3-read_file_async');
 
+// Define the app
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -14,16 +16,16 @@ const app = http.createServer((req, res) => {
   }
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    countStudents(process.argv[2].toString()).then((output) => {
-      const outString = output.slice(0, -1);
-      res.end(outString);
-    }).catch(() => {
+    countStudents(process.argv[2]).then((data) => {
+      res.end(data.join('\n'));
+    }).catch((err) => {
       res.statusCode = 404;
-      res.end('Cannot load the database');
+      res.end(`${err.message}`);
     });
   }
 });
 
+// Display server info
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
