5be07d7e3e24fa4d3e9ba51d3f8cf92f636b2b5e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-14 16:06:28 +0100

    Fix 1 for task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,11 +1,11 @@
 const readDatabase = require('../utils');
-const process = require('process');
+const argv = process.argv;
 
 class StudentsController {
   static getAllStudents(req, res) {
-    readDatabase(process[2]).then((data) => {
+    readDatabase(argv[2]).then((data) => {
       const logs = ['This is the list of our students'];
-      for (const [k, v] in Object.entries(data)) {
+      for (const [k, v] of Object.entries(data)) {
         logs.push(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
       }
       res.status(200).send(logs.join('\n'));
@@ -16,13 +16,11 @@ class StudentsController {
 
   static getAllStudentsByMajor(req, res) {
     const field = req.params.major;
-    const acceptField = ['CS', 'SWE'];
-
-    if (!acceptField.includes(field)) {
+    const acceptedMajors = ['CS', 'SWE'];
+    if (!acceptedMajors.includes(field)) {
       res.status(500).send('Major parameter must be CS or SWE');
     }
-
-    readDatabase(process[2]).then((data) => {
+    readDatabase(argv[2]).then((data) => {
       res.status(200).send(`List: ${data[field].join(', ')}`);
     }).catch(() => {
       res.status(500).send('Cannot load the database');
