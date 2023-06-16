ca09e8acb3a25297a36dfbe09f759346c6027e3f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-16 11:55:17 -0700

    Added test for task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a promise', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).to.be.an('object');
+        expect(data).to.have.property('data');
+        expect(data).to.deep.equal({data: 'Successful response from the API' });
+        done();
+      });
+  });
+});
